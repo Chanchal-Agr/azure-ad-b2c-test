97e97da8d645526d5575a3a7175cf1e7dc8c5605
Chanchal-Agr <chanchalagrawal1409@gmail.com>
2025-01-23 09:51:16 +0530

    move login logic into login service

--- src/app/components/core/nav-bar/nav-bar.component.ts
+++ src/app/components/core/nav-bar/nav-bar.component.ts
@@ -6,6 +6,7 @@ import { filter } from 'rxjs';
 import { LocalStorageService } from '../../../services/local-storage.service';
 import { environment } from '../../../../environments/environment';
 import { Token } from '../../../models/token';
+import { LoginService } from '../../../services/login.service';
 
 
 @Component({
@@ -15,72 +16,23 @@ import { Token } from '../../../models/token';
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
-  public loginDisplay = false;
+  public loginDisplay:boolean = false;
 
-  constructor(
-    @Inject(MSAL_GUARD_CONFIG) private msalGuardConfig: MsalGuardConfiguration,
-    private authService: MsalService,
-    private msalBroadcastService: MsalBroadcastService,
-    private localStorage: LocalStorageService
-  ) {
-
-  }
+  constructor(private loginService: LoginService) { }
 
   ngOnInit(): void {
-    this.isUserLogin();
-
-    this.msalBroadcastService.msalSubject$
-      .pipe(
-        filter(
-          (msg: EventMessage) =>
-            msg.eventType === EventType.ACCOUNT_ADDED ||
-            msg.eventType === EventType.ACCOUNT_REMOVED ||
-            msg.eventType === EventType.LOGIN_SUCCESS // Add this event type
-        )
-      )
-      .subscribe((result: EventMessage) => {
-        if (this.authService.instance.getAllAccounts().length === 0) {
-          window.location.pathname = '/';
-        } else {
-          this.isUserLogin();
-        }
-      });
+    this.loginService.isLoggedIn$.subscribe((loggedIn: boolean) => {
+      this.loginDisplay = loggedIn;
+    });
   }
-
   public loginRedirect() {
-    console.log("Login method called.");
-    if (this.msalGuardConfig.authRequest) {
-      this.authService.loginRedirect({
-        ...this.msalGuardConfig.authRequest,
-      } as RedirectRequest).subscribe(() => {
-        this.isUserLogin(); // Update loginDisplay after successful login
-      });
-    } else {
-      this.authService.loginRedirect().subscribe(() => {
-        this.isUserLogin(); // Ensure loginDisplay is updated here too
-      });
+    this.loginService.loginRedirect()
+  }
+
+  public logout() {
+    this.loginService.logout()
     }
-  }
 
-
-  public logout(popup?: boolean) {
-    if (popup) {
-      this.authService.logoutPopup({
-        mainWindowRedirectUri: '/',
-      });
-    } else {
-      this.authService.logoutRedirect();
-    }
-  }
-
-
-  isUserLogin() {
-    let identifier: string = environment.adb2cConfig.clientId;
-    let token = this.localStorage.getItem<Token | null>('msal.token.keys.'+ identifier);
-    // this.loginDisplay = this.authService.instance.getAllAccounts().length > 0;
-    this.loginDisplay = token != null && token.accessToken.length > 0;
-    console.log(`Login display-${!this.loginDisplay}`);
-  }
 }
 
  
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject, firstValueFrom } from 'rxjs';
+import { Inject, Injectable } from '@angular/core';
+import { BehaviorSubject, firstValueFrom, Observable } from 'rxjs';
 
-import { MsalService, MsalBroadcastService } from '@azure/msal-angular';
-import { EventMessage, AuthenticationResult, InteractionStatus, EventType } from '@azure/msal-browser';
+import { MsalService, MsalBroadcastService, MsalGuardConfiguration, MSAL_GUARD_CONFIG } from '@azure/msal-angular';
+import { EventMessage, AuthenticationResult, InteractionStatus, EventType, RedirectRequest } from '@azure/msal-browser';
 import { filter } from 'rxjs/operators';
 
 import { Claim } from '../models/claim';
@@ -21,21 +21,32 @@ export class LoginService {
   private claimsSubject = new BehaviorSubject<Claim[]>([]);
   claims$ = this.claimsSubject.asObservable();
 
-  loginDisplay = false;
+  loginDisplay:boolean = false;
   displayedColumns: string[] = ['claim', 'value', 'description'];
+  private loggedInSubject = new BehaviorSubject<boolean>(false);
+  isLoggedIn$: Observable<boolean> = this.loggedInSubject.asObservable();
 
-  constructor(private authService: MsalService, private msalBroadcastService: MsalBroadcastService, private http: HttpClient, private localStorage: LocalStorageService) {
+  constructor(
+    private authService: MsalService,
+     private msalBroadcastService: MsalBroadcastService, 
+     private http: HttpClient,
+      private localStorage: LocalStorageService,
+     @Inject(MSAL_GUARD_CONFIG) private msalGuardConfig: MsalGuardConfiguration) {
     console.log("Constructor called from login service.");
     this.msalBroadcastService.msalSubject$
       .pipe(
-        filter((msg: EventMessage) => msg.eventType === EventType.LOGIN_SUCCESS),
+        filter((msg: EventMessage) =>
+          msg.eventType === EventType.LOGIN_SUCCESS
+        )
       )
       .subscribe((result: EventMessage) => {
         const payload = result.payload as AuthenticationResult;
         console.log("Set active account");
 
         this.authService.instance.setActiveAccount(payload.account);
+        this.setLoginDisplay(); 
       });
+
 
     this.msalBroadcastService.inProgress$
       .pipe(
@@ -49,10 +60,41 @@ export class LoginService {
 
       });
   }
+ public loginRedirect() {
+    console.log("Login method called.");
+    if (this.msalGuardConfig.authRequest) {
+      this.authService.loginRedirect({
+        ...this.msalGuardConfig.authRequest,
+      } as RedirectRequest).subscribe(() => {
+        this.setLoginDisplay(); // Update loginDisplay after successful login
+      
+      });
+    } else {
+      this.authService.loginRedirect().subscribe(() => {
+        this.setLoginDisplay(); // Ensure loginDisplay is updated here too
+      });
+    }
+  }
 
+
+  public logout() {
+    console.log("log out called");
+
+      this.authService.logoutRedirect();
+    }
+
+    get isLoggedIn(): boolean {
+      return this.loggedInSubject.getValue();
+    }
 
   setLoginDisplay() {
-    this.loginDisplay = this.authService.instance.getAllAccounts().length > 0;
+    // this.loginDisplay = this.authService.instance.getAllAccounts().length > 0;
+    let identifier: string = environment.adb2cConfig.clientId;
+    let token = this.localStorage.getItem<Token | null>('msal.token.keys.'+ identifier);
+    // this.loginDisplay = this.authService.instance.getAllAccounts().length > 0;
+    this.loginDisplay = token != null && token.accessToken.length > 0;
+    console.log(`Login display-${!this.loginDisplay}`);
+    this.loggedInSubject.next(this.loginDisplay);
   }
 
   getClaims(claims: any) {
